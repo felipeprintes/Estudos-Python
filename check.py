fc5194806acf0ba0d0bb8e7223bb945eada1917c
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-26 08:56:14 -0300

    check

--- check.py
+++ check.py
@@ -2,10 +2,8 @@ nome = input("Entre com seu nome: ")
 
 def entradaEmail():
 	email = input("Entre com seu email: ")
-	return email
-entradaEmail()
 
-
+entradaEmail()				
 		
 senha = input("Entre com sua senha: ")
 conf_senha = input("Confirme sua senha: ")
@@ -13,3 +11,6 @@ conf_senha = input("Confirme sua senha: ")
 while senha != conf_senha:
 	print("Senha inválida, tente de novo")
 	conf_senha = input("Confirme sua senha: ")	
+
+if senha == conf_senha:
+	print("Seja bem vindo!")
