d23d898dcb3cdd90162517ed7a1070b8882c94bc
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-27 15:29:31 -0300

    exercicio uri

--- dinner.py
+++ dinner.py
@@ -3,14 +3,16 @@ n,d = map(int, s.split())
 n = int(n)
 d = int(d)
 soma = 0
-
+aux = 0
 for i in range(d):
 	x = input()
 	x = list(map(int,x.split()))
-
-for j in range(d):
-	soma = soma + x[j]
+	soma = sum(x)
 	if soma == n:
-		print("yes")
-		break
-		
+		aux = soma
+if soma == aux:
+	print("Yes")
+else:
+	print("no")			
+
+	
